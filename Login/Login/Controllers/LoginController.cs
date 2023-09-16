5401ffb343f959881cdb2f9d3a105396e5fdb1fe
Guilherme Ferreira <guilherme.ferreira28@outlook.com>
2023-09-16 16:20:32 -0300

    ADD jwt token

--- Login/Login/Controllers/LoginController.cs
+++ Login/Login/Controllers/LoginController.cs
@@ -1,8 +1,12 @@
 using Login.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net;
 using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
@@ -11,16 +15,20 @@ namespace Login.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+    [Authorize]
     public class LoginController : ControllerBase
     {
         private readonly ILogger<LoginController> _logger;
 
         private readonly IHttpContextAccessor httpContextAccessor;
 
-        public LoginController(ILogger<LoginController> logger, IHttpContextAccessor _httpContextAccessor)
+        private readonly IConfiguration configuration;
+
+        public LoginController(ILogger<LoginController> logger, IHttpContextAccessor _httpContextAccessor, IConfiguration _configuration)
         {
             _logger = logger;            
             httpContextAccessor = _httpContextAccessor;
+            configuration = _configuration;
         }
 
         public static string Hash(string key)
@@ -39,36 +47,52 @@ namespace Login.Controllers
             return Sb.ToString();
         }
 
+        [HttpGet]
+        [Route("Logout")]
+        public async void Logout()
+        {
+            var context = httpContextAccessor.HttpContext;
+
+
+        }
+
         [HttpPost]
         [Route("Login")]
-        public async void Login([FromBody] Credentials credentials)
+        [AllowAnonymous]
+        public IActionResult Login([FromBody] Credentials credentials)
         {
 
-            var context = httpContextAccessor.HttpContext;
-            var username = credentials.Username;
-            var passwordHash = Hash(credentials.Password);
-
-            //var user = new User
-            //{
-            //    Email = username,
-            //    PasswordHash = passwordHash,
-            //};
-
-            //if (!userRepository.IsValidUser(user))
-            //{
-            //    throw new Exception();
-            //}
-
-            var claims = new List<Claim>
+            if (credentials.Username == "guilherme" && credentials.Password == "derteufel")
             {
-                new Claim(ClaimTypes.Name, credentials.Username)
-            };
-            var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
 
-            await context.SignInAsync(
-                CookieAuthenticationDefaults.AuthenticationScheme,
-                new ClaimsPrincipal(identity)
-                );
+                var issuer = configuration["Jwt:Issuer"];
+                var audience = configuration["Jwt:Audience"];
+                var key = Encoding.ASCII.GetBytes
+                (configuration["Jwt:Key"]);
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new[]
+                    {
+                        new Claim("Id", Guid.NewGuid().ToString()),
+                        new Claim(JwtRegisteredClaimNames.Sub, credentials.Username),
+                        new Claim(JwtRegisteredClaimNames.Email, credentials.Username),
+                        new Claim(JwtRegisteredClaimNames.Jti,
+                        Guid.NewGuid().ToString())
+                    }),
+                    Expires = DateTime.UtcNow.AddMinutes(5),
+                    Issuer = issuer,
+                    Audience = audience,
+                    SigningCredentials = new SigningCredentials
+                    (new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha512Signature)
+                };
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                var jwtToken = tokenHandler.WriteToken(token);
+                var stringToken = tokenHandler.WriteToken(token);
+                return Ok(stringToken);
+            }
+            return Unauthorized();
         }
     }
 }
--- Login/Login/Program.cs
+++ Login/Login/Program.cs
@@ -1,8 +1,23 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowOrigin",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+        });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -11,18 +26,34 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddHttpContextAccessor();
 
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(options =>
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(o =>
+{
+    o.TokenValidationParameters = new TokenValidationParameters
     {
-        options.Events.OnRedirectToLogin = (context) =>
-        {
-            context.Response.StatusCode = 401;
-            return Task.CompletedTask;
-        };
-        options.Cookie.Name = "LoginI";
-    });
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey
+        (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = false,
+        ValidateIssuerSigningKey = true
+    };
+});
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
+
+app.UseCors(x => x
+              .AllowAnyMethod()
+              .AllowAnyHeader()
+              .AllowAnyOrigin()); // allow credentials
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -32,6 +63,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
